98fd2959e54a109cd967f4eca1d20761d781487d
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2025-10-26 12:01:29 +0100

    dialogos level3 & finales

--- Enumera-t/Assets/Scripts/Dialogue/StartDialogueTrigger.cs
+++ Enumera-t/Assets/Scripts/Dialogue/StartDialogueTrigger.cs
@@ -6,24 +6,25 @@ public class StartDialogueTrigger : MonoBehaviour
 {
 
     public Dialogo startDialogue;
+    public Dialogo endDialogue;
     private float contador = 0;
     private bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        DialogueManager.instance.StartDialogue(startDialogue);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (contador > 0.5) {
+        if (contador > 0.1) {
             if (!hasStarted)
             {
                 hasStarted = true;
-                DialogueManager.instance.StartDialogue(startDialogue);
+                
             }
         }
         else
